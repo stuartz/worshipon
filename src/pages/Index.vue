875398751403e02aff05f1ecb210495d556fed4f
Stuart Zurcher <stuartz.ccrx@gmail.com>
2020-03-29 14:24:04 -0500

    added files

---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="WorshipOn logo"
+      src="~assets/beach-1851101.jpg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
